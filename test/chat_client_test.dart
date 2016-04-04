2684ce1377120e5c039a5fd2803f6d72609ddc89
Matteo Rosati <mrosati@h-art.com>
2016-04-04 22:38:54 +0200

    add unit test for the ChatClient class

---
+++ test/chat_client_test.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import 'dart:io';
+
+/// The class to be tested
+import '../lib/classes/chat_client.dart';
+
+/// Mock class to use in [ChatClient] class.
+class MockWebSocket extends Mock implements WebSocket {}
+
+void main() {
+  test("A chat client can be added and removed from clients list", () {
+    ChatClient client = new ChatClient(new MockWebSocket());
+    client.addToClientsList();
+    expect(chatClientList.length, equals(1));
+
+    client.removeFromClientsList();
+    expect(chatClientList.length, equals(0));
+  });
+}
